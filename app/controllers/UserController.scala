5388d6686265f5b719c656613dc9ba4ae7d12637
tono0326 <tono0326@gmail.com>
2015-09-17 00:25:16 +0900

    first commit

---
+++ app/controllers/UserController.scala
@@ -0,0 +1,53 @@
+package controllers
+
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.i18n.{MessagesApi, I18nSupport}
+import play.api.db.slick._
+import slick.driver.JdbcProfile
+import models.Tables._
+import javax.inject.Inject
+import scala.concurrent.Future
+import slick.driver.H2Driver.api._
+
+/**
+ * @author cw_ono
+ */
+class UserController @Inject()(val dbConfigProvider: DatabaseConfigProvider,
+                               val messagesApi: MessagesApi) extends Controller 
+    with HasDatabaseConfigProvider[JdbcProfile] with I18nSupport {
+  
+  /**
+   * 一覧表示
+   */
+  // implicit rsはアクションの処理の中でHTTPリクエストやDBのセッションを暗黙的に使用するために必要になる記述
+  def list = Action.async { implicit rs =>
+    // IDの昇順に全てのユーザー情報を取得(SELECT * FROM USERS ORDER BY ID)
+    db.run(Users.sortBy(t => t.id).result).map { users =>
+      // 一覧画面を表示
+      Ok(views.html.user.list(users))
+    }
+  }
+  
+  /**
+   * 編集画面表示
+   */
+  def edit(id: Option[Long]) = TODO
+  
+  /**
+   * 登録実行
+   */
+  def create = TODO
+  
+  /**
+   * 更新実行
+   */
+  def update = TODO
+  
+  /**
+   * 削除実行
+   */
+  def remove(id: Long) = TODO
+}
