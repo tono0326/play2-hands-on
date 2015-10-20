6afb83d70b346765cb80ea4f7e51d3bc81f8b098
cw-ono <ono@chatwork.com>
2015-10-21 01:13:40 +0900

    うまく動かないが一旦コミット

--- play2-hands-on/README.md
+++ play2-hands-on/README.md
@@ -14,3 +14,47 @@ This file will be packaged with your application, when using `activator dist`.
 
 [PlayFramework - activator eclipse で [error] Not a valid command: eclipse (similar: help, alias) の時の対処法 - Qiita](http://qiita.com/shogo807/items/e50b4538bb1964d4ee92)
 
+## 動かし方
+
+Play
+
+```sh
+$ cd ~/scala-play/play2-hands-on/play2-hands-on/
+$ activator run
+```
+
+DB
+
+```sh
+$ cd ~/scala-play/play2-hands-on/slick-codegen/h2/
+$ sh start.sh
+```
+
+h2-browser
+
+[Developing-with-the-H2-Database](https://www.playframework.com/documentation/ja/2.3.x/Developing-with-the-H2-Database)
+
+```sh
+$ activator
+h2-browser
+```
+
+# 課題
+- 項目を増やす
+    - メールアドレス
+    - パスワード
+- 複数の更新・削除ができる
+- 削除のIDをDBチェック
+
+# その他
+ディレクトリ階層を増やした
+
+```sh
+$ find . -type d -d 1 ! -path "./.*" ! -path "./play2-hands-on" | sed 's!^.*/!!' | xargs -I % git mv ./% ./play2-hands-on/%
+$ find . -type f -d 1 ! -path "./.*" | sed 's!^.*/!!' | xargs -I % git mv ./% ./play2-hands-on/%
+```
+
+# 参考資料
+- [Scala - Playframeworkのフォームで良く使うバリデーション - Qiita](http://qiita.com/modal_soul/items/00da5105b29880fee590)
+- [Scala ExecutionContextって何 / Futureはスレッド立ち上げじゃないよ - ましめも](http://mashi.hatenablog.com/entry/2014/11/24/010417)
+
---
+++ play2-hands-on/app/controllers/LoginController.scala
@@ -0,0 +1,49 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.data.validation._
+
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.i18n.{MessagesApi, I18nSupport}
+import play.api.db.slick._
+import slick.driver.JdbcProfile
+import models.Tables._
+import javax.inject.Inject
+import scala.concurrent.Future
+import slick.driver.H2Driver.api._
+
+class LoginController @Inject()(val dbConfigProvider: DatabaseConfigProvider,
+                               val messagesApi: MessagesApi) extends Controller 
+    with HasDatabaseConfigProvider[JdbcProfile] with I18nSupport {
+
+  import LoginController._ // コンパニオンオブジェクトに定義したFormを参照
+  def index = Action.async { implicit rs =>
+    // val form = loginForm
+    // Ok(views.html.board.login(loginForm))
+    // form.flatMap { form => Ok(views.html.board.login(form))}
+    // val form = loginForm.fill(LoginForm("test@test.com", "hogehoge"))
+    // val form = Future { loginForm }
+    val form = loginForm
+    Ok(views.html.board.login(form))
+  }
+  
+  // ログイン処理
+  def login = TODO
+
+}
+
+object LoginController {
+  case class LoginForm(email: String, password: String)
+  
+  val halfWidthAlphaNum = Constraints.pattern("[a-zA-Z0-9]+".r)
+  
+  var loginForm = Form(
+      mapping(
+          "email" -> email,
+          "password" -> nonEmptyText.verifying(halfWidthAlphaNum)
+      )(LoginForm.apply)(LoginForm.unapply)
+  )
+}
